ae6e41d15f00d56ba4ee43a309f6df0b3b253c6b
vebervi <vincent.veber@cpe.fr>
2021-05-25 12:05:38 +0200

    Atelier 2 - Tests unitaires

--- card/src/test/java/com/cpe/card/card/dto/UserDTOTest.java
+++ card/src/test/java/com/cpe/card/card/dto/UserDTOTest.java
@@ -1,12 +1,28 @@
 package com.cpe.card.card.dto;
 
-import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class UserDTOTest {
 
 	@Test
 	public void createUserDTO() {
-		// TODO
+		UserDTO user = new UserDTO(1, "test", "pwd", 50);
+		
+		assertTrue(user.getId() == 1);
+		assertTrue(user.getSurname() == "test");
+		assertTrue(user.getPassword() == "pwd");
+		assertTrue(user.getMoney() == 50);
+		
+		user.setId(5);
+		user.setSurname("testBis");
+		user.setPassword("pwdBis");
+		user.setMoney(15);
+		
+		assertTrue(user.getId() == 5);
+		assertTrue(user.getSurname() == "testBis");
+		assertTrue(user.getPassword() == "pwdBis");
+		assertTrue(user.getMoney() == 15);
 	}
-	
 }
