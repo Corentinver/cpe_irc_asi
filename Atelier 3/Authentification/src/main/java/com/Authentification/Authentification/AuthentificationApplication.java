c3a59c30b382fb526c83e3f7068672259915a9a6
Corentin Vergnon <corentin.vergnon@tpc31.cpe.fr>
2021-05-27 17:19:11 +0200

    start atelier 3

---
+++ Atelier 3/Authentification/src/main/java/com/Authentification/Authentification/AuthentificationApplication.java
@@ -0,0 +1,15 @@
+package com.Authentification.Authentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import common.UserDTO;
+
+@SpringBootApplication
+public class AuthentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentificationApplication.class, args);
+	}
+
+}
