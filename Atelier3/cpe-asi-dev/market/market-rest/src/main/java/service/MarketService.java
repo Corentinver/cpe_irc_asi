5da5d7796e79a3272f45cd6a64cf0a2d513d3702
Corentin Vergnon <corentin.vergnon@tpopt29.cpe.fr>
2021-06-07 09:56:31 +0200

    market

---
+++ Atelier3/cpe-asi-dev/market/market-rest/src/main/java/service/MarketService.java
@@ -0,0 +1,18 @@
+package service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MarketService {
+	
+	public Boolean buyCard(int userId, int cardId) {	
+		return null;
+	}
+	
+	public Boolean sellCard(int userId, int cardId) {
+		return null;
+	}
+	
+	
+	
+}
