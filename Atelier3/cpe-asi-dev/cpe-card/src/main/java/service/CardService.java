dec1bbfacb5c466e258ab1cda4f29fafcabdb37e
vebervi <vincent.veber@cpe.fr>
2021-06-08 10:08:58 +0200

    Atelier 3 - Card Microservice

--- Atelier3/cpe-asi-dev/cpe-card/src/main/java/service/CardService.java
+++ Atelier3/cpe-asi-dev/cpe-card/src/main/java/service/CardService.java
@@ -1,7 +1,6 @@
 package service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,78 +17,44 @@ public class CardService {
 	
 	@Autowired
 	private CardRepository cardRepository;
-
-	private UserRest userRest;
+	
+	private RestTemplate restTemplate = new RestTemplate();
 	
 	public List<Card> getCollectionCardsByUser(int userId) {
-		RestTemplate restTemplate = new RestTemplate();
+		String url = UserRest.SERVER + UserRest.BASE_URL + UserRest.USER_URL;
+		url = url.replace("{id}", String.valueOf(userId));
+		
+		User user = restTemplate.getForObject(url, User.class);
 
-		String url = userRest.SERVER + userRest.BASE_URL + userRest.USER_URL;
+		return this.getCardsFromUser(user);
+	}
+
+	public List<Card> getShopCardsByUser(int userId) {
+		String url = UserRest.SERVER + UserRest.BASE_URL + UserRest.USER_URL;
 		url = url.replace("{id}", String.valueOf(userId));
 		
 		User user = restTemplate.getForObject(url, User.class);
 		
-		System.out.print(user);
-		System.out.print(user.getName());
-
-		return new ArrayList<Card>();
-	}
-
-	public List<Card> getShopCardsByUser(int userId) {
-		/*
-		ResponseEntity response = userImpl.getUserById(userId);
-		
-		// TODO : il ne devrait pas y avoir d'implémentation des controlleurs dans le common.
-		// On devrait plutôt passer par les interfaces du common.
-		
-		if(response.getStatusCodeValue() != 200) {
-			return null;
-		}
-		
-		User user = (User) response.getBody();
-		List<Card> cards = getCardFromUser(user);
+		List<Card> ownedCards = this.getCardsFromUser(user);
 		List<Card> allCards = cardRepository.findAll();
-		allCards.removeAll(cards);
 		
-		/*
-		RestTemplate restTemplate = new RestTemplate();
-		User user = restTemplate.getForObject("http://localhost:8080/user/" + userId, User.class);
-		System.out.print(user);
+		allCards.removeAll(ownedCards);
 		
-		/*
-		RestTemplate restTemplate = new RestTemplate();
-		User user = restTemplate.getForObject("/users/" + id, User.class);
-		List<Card> listOwnedCards = user.getCards();
-		List<Card> listAllCards = cardRepository.findAll();
-		listAllCards.removeAll(listOwnedCards);
-		return listAllCards;
-		*/
-		
-		/*
-		User user = userService.findById(id);
-		List<Card> listOwnedCards = user.getCards();
-		List<Card> listAllCards = cardRepository.findAll();
-		listAllCards.removeAll(listOwnedCards);
-		return listAllCards;*/
-		//return allCards;
-		return null;
+		return allCards;
 	}
 	
 	public Card getCardById(int id) {
 		return cardRepository.findById(id).get();
 	}
 	
-	private List<Card> getCardFromUser(User user){
-		List<Card> cards = new ArrayList();
-		List<Integer> cardsInt = Arrays.asList(user.getCards());
-		cardsInt = new ArrayList(cardsInt);
-		cardsInt.forEach(cardId -> {
-			Card card = cardRepository.getById(cardId);
+	private List<Card> getCardsFromUser(User user){
+		List<Card> cards = new ArrayList<Card>();
+		for(Integer cardId : user.getCards()) {
+			Card card = this.getCardById(cardId);
 			if(card != null)
 				cards.add(card);
-		});
+		}
 		
 		return cards;
-		
 	}
 }
